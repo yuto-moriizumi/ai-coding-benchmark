619fc95d6fb39627af157afb9d6fb73e64c00619
yuto-moriizumi <kurvan1112@gmail.com>
2025-07-02 23:25:03 +0900

    テストが自動で終了するように修正

--- uat/playwright.config.ts
+++ uat/playwright.config.ts
@@ -8,10 +8,9 @@ const config: PlaywrightTestConfig = {
     timeout: 5000,
   },
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  retries: 0,
+  workers: 1,
+  reporter: [["html", { open: "never" }]],
   use: {
     actionTimeout: 0,
     trace: "on-first-retry",
